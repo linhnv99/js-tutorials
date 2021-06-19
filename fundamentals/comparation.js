2da447fdb44c29584a064595ce23b211ab2dde0e
LinhNV <nvlk12haui@gmail.com>
2021-06-19 13:49:21 +0700

    comparation

---
+++ fundamentals/comparation.js
@@ -0,0 +1,14 @@
+console.log("===========STRING COMPARASION============");
+
+console.log('"A" > "Z"', "A" > "Z"); // false
+console.log('"a" < "b"', "a" < "b"); // true
+console.log('"linha" > "linhb"', "linha" > "linhb"); // false
+// compare by alphabet  
+
+
+
+console.log("1" == 1); // true
+console.log(true == 1); 
+console.log("true" ==  1); // false
+
+console.log(undefined == null); // null only equal undefined
