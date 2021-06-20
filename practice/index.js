4cf13c16fb684249c626376863035bbbb351abc7
LinhNV <nvlk12haui@gmail.com>
2021-06-20 22:18:07 +0700

    practice

---
+++ practice/index.js
@@ -0,0 +1,227 @@
+const myQuestions = [
+    {
+        question: "Javascript is _______ language.",
+        answers: {
+            a: "Programing",
+            b: "Application",
+            c: "Nont of These",
+            d: "Scripting",
+        },
+        multi: false,
+        correctAnswer: "d"
+    },
+    {
+        question:
+            "Which of the following is a valid type of function javascript supports?",
+        isChecked: "",
+        answers: {
+            a: "named function",
+            b: "anonymous function",
+            c: "both of the above",
+            d: "none of the above",
+        },
+        multi: false,
+        correctAnswer: "c"
+    },
+    {
+        question:
+            "Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
+        answers: {
+            a: "getIndex()",
+            b: "location()",
+            c: "indexOf()",
+            d: "getLocation()",
+        },
+        multi: false,
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of the following is valid data type of JavaScript",
+        answers: {
+            a: "number",
+            b: "void",
+            c: "boolean",
+            d: "nothing",
+        },
+        multi: false,
+        correctAnswer: "c"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: {
+            a: "//This is comment",
+            b: "'This is comment",
+            c: "<!--This is comment-->",
+            d: "nothing",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "<script>",
+            b: "<scripting>",
+            c: "<js>",
+            d: "<javascipt>",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: {
+            a: "The <head> section",
+            b: "The <body> section",
+            c: "a & b",
+            d: "none of the above",
+        },
+        multi: false,
+        correctAnswer: "c"
+    },
+    {
+        question:
+            "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answers: {
+            a: "<script href = 'xxx.js'>",
+            b: "<script src = 'xxx.js'>",
+            c: "<script name ='xxx.js'>",
+            d: "nothing",
+        },
+        multi: false,
+        correctAnswer: "b"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: {
+            a: "alert('Hello World')",
+            b: "msgBox('Hello World')",
+            c: "msg('Hello World')",
+            d: "alertBox('Hello World')",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: {
+            a: "function:myFunction()",
+            b: "function myFunction()",
+            c: "function = myFunction()",
+            d: "none of the above",
+        },
+        multi: false,
+        correctAnswer: "b"
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        answers: {
+            a: "call myFunction()",
+            b: "myFunction()",
+            c: "call function myFunction()",
+            d: "none of the above",
+        },
+        multi: false,
+        correctAnswer: "b"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: {
+            a: "if(i == 5)",
+            b: "if i == 5 then",
+            c: "if i = 5",
+            d: "if i = 5 then",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question:
+            "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        answers: {
+            a: "if(i != 5)",
+            b: "if i != 5 then",
+            c: "if i <> 5",
+            d: "if(i <> 5)",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question: "How does a WHILE loop start?",
+        answers: {
+            a: "while (i <=10; i++)",
+            b: "while (i <= 10)",
+            c: "while i = 1 to 10",
+            d: "none of the above",
+        },
+        multi: false,
+        correctAnswer: "b"
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: {
+            a: "for (i <= 5; i++)",
+            b: "for (i=0 ; i <= 5; i++)",
+            c: "for (i=0 ; i++)",
+            d: "for i to 5",
+        },
+        multi: false,
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: {
+            a: "var colors = ['red','green','blue','yellow']",
+            b: "var colors = 'red','green','blue','yellow'",
+            c: "var colors = ('1:red','2: green','3: blue','4: yellow')",
+            d: "nothing",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        answers: {
+            a: "rnd(7.25)",
+            b: "Math.rnd(7.25)",
+            c: "round(7.25)",
+            d: "Math.round(7.25)",
+        },
+        multi: false,
+        correctAnswer: "d"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        answers: {
+            a: "Math.max(X,y)",
+            b: "Mail.ceil(x,y)",
+            c: "top(x,y)",
+            d: "ceil(x,y)",
+        },
+        multi: false,
+        correctAnswer: "a"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: {
+            a: "onchange",
+            b: "onclick",
+            c: "onmouseover",
+            d: "onmouseclick",
+        },
+        multi: false,
+        correctAnswer: "b"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: {
+            a: "-",
+            b: "*",
+            c: "x",
+            d: "=",
+        },
+        multi: false,
+        correctAnswer: "d"
+    },
+];
