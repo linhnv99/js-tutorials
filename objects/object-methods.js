32244199a62db027748e7f3a7e420f7f1fe0af02
LinhNV <nvlk12haui@gmail.com>
2021-06-20 22:17:54 +0700

    object

---
+++ objects/object-methods.js
@@ -0,0 +1,124 @@
+
+// Note: This trong JS khác so với hầu hết các ngôn ngữ khác, 
+// nó có thể tồn tại ở bất cứ hàm nào ngay cả khi nó k phải là 1 phương thức của obj.
+
+/*
+    This: là 1 keyword (là 1 ngữ cảnh) trong:
+    
+    + Method context: 
+        this là chủ thể của đối tương chứa method đó. 
+    vd: 
+        let user = {
+            name: "Linh",
+            myFunc: funtion () { // call method
+                console.log(this.name)// this = user
+            }
+        }
+    + Funtion context:
+        this đại diện cho global context (window), this phụ thuộc vào cách mà funtion được gọi.
+    + Class context: 
+        same as Java
+
+
+*/
+
+
+// - Funtion trong JS được coi là 1 object đăc biệt chúng đều có các method call, apply, bind. 
+
+// 1. Bind()
+
+let animals = {
+    name: "CAT",
+    age: 20,
+    getInfo: function () {
+        console.log("Name: ", this.name, " - ", this.age);
+    }
+}
+
+let logAnimal = function (name) {
+    console.log("- Before changing: " + this.name);
+
+    this.name = name; // changed name 
+
+    console.log("- After changing: " + this.name);
+}
+
+animals.getInfo(); // Name:  CAT  -  20
+
+let log = logAnimal.bind(animals); // tạo ra 1 instance của logAnimal và ràng buộc với animals. gần như thêm 1 pro cho 1 obj
+// có thể thay đổi thuộc tính của 1 obj
+
+console.log("Log: ");
+log("DOG");
+
+animals.getInfo(); // Name:  DOG  -  20
+
+
+
+
+
+
+
+
+
+console.log("=================PRACTICE===============");
+// Prac1.
+
+function makeUser() {
+    return {
+        name: "John",
+        ref: this
+    };
+}
+/**
+ *  Same as 
+ * function makeUser() {
+        return this;
+    }
+ * makeUser().name; // undefined
+ */
+
+let user = makeUser();
+console.log("use call func: ", user.ref.name); // What's the result? // undefined
+console.log("use call func: ", user1.ref); // window
+let user1 = new makeUser();
+console.log("use new Object: ", user1.name);
+console.log("use new Object: ", user1.ref);
+
+//Rs: this ở đây là thể hiện của 1 hàm 
+
+// 
+function makeUser1() {
+    return {
+        name: "John",
+        ref() {
+            return this;
+        }
+    };
+}
+
+let user2 = makeUser1();
+
+console.log(user2.ref().name); // John
+
+
+
+
+// Prac2
+let u = {
+    name: "Linh",
+    ref: this
+}
+console.log(u.ref);
+
+
+// Prac3
+let obj = {};
+
+function A() { return obj }
+function B() { return obj }
+
+let a = new A();
+let b = new B();
+
+console.log(a == b); // true
---
+++ objects/object.js
@@ -0,0 +1,73 @@
+
+// 2 cách để khởi tạo object
+
+let user1 = new Object(); // constructor
+let user2 = {}; // literal
+
+let student = {
+    name: "Linh",
+    age: 20,
+    skills: ["Java", "Js", "ReactJs"],
+    myFuntion: function () {
+        console.log("My name is " + this.name + " - " + this.age);
+    }
+}
+
+student.key1 = "new Value1"; // add properties for the object
+student.key2 = "new Value2";
+
+console.log(student.key1);
+console.log(student.name);
+console.log(student["age"]);
+
+student.myFuntion();
+
+// loop for key object
+for (let key in student) {
+    console.log("Key of student: ", key);
+}
+
+
+
+// việc gán 1 obj khác với kiểu dữ liệu nguyên thủy
+// gán tham chiếu. 2(n) biến có thể tham chiếu vào cùng 1 ô nhớ.
+// bất kì biến tham chiếu nào cũng có thể thay đổi giá trị của ô nhớ đó.
+
+let user = { name: 'John' };
+
+console.log("user.name = ", user.name);
+
+let admin = user;
+
+admin.name = 'Pete'; // changed by the "admin" reference
+
+console.log("user.name = ", user.name);
+console.log("admin.name = ", admin.name);
+
+console.log("================Cloning Object ... ================");
+
+user = {
+    name: "John",
+    age: 30
+};
+
+let clone = {}; // the new empty object
+
+// let's copy all user properties into it
+for (let key in user) {
+    clone[key] = user[key];
+}
+
+// now clone is a fully independent object with the same content
+clone.name = "Pete"; // changed the data in it
+
+console.log(user.name); // still John in the original object
+
+
+
+console.log("====Use Object.assign(dest, src1, ...srcn) to copy object====");
+let cloneObj = {};
+Object.assign(cloneObj, user);
+console.log(cloneObj)
+
+
