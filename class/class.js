4bbf2981cc85e15c5b3b5d47698425cb1048ccb5
LinhNV <nvlk12haui@gmail.com>
2021-06-30 22:57:55 +0700

    class

---
+++ class/class.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+    sayHi() {
+        console.log("say hi");
+    }
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+}
+// Khi 1 instance của 1 class được khởi tạo. các property và method của class sẽ đc lưu tại prototype
+let user = new User("Linh");
+
+console.log(User.prototype.constructor === User);
+console.log(User.prototype.sayHi == user.sayHi);
+
+console.log(user.name);
+
+
